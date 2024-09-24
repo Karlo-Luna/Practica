2a4a530e273be18b1377961c5cfdbaf17a7aa427
Karlo con K <karlourieluna2@outlook.com>
2024-09-23 20:19:02 -0600

    Create Programa2.cpp
    Programa de edad

---
+++ Programa2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int edad;
+    printf("¿Qué edad tienes?\n");
+    scanf_s("%i",&edad);
+    if(edad>=18){
+        printf("Ya tienes edad para votar");
+    } 
+}
